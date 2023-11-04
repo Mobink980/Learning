b882a52951384ed366c59d60ef0f9feef160aae9
Ali Karazmoodeh <mobink980@gmail.com>
2023-11-04 07:58:10 +0330

    Add files via upload

--- programs/CardioGoodFitness.py
+++ programs/CardioGoodFitness.py
@@ -1,2 +1,31 @@
-import numpy as np
+# This code is awesome. It generates a random dataset
 import pandas as pd
+import numpy as np
+
+data = []
+for i in range(300):
+    age = np.random.randint(18, 100)
+    workclass = np.random.choice(['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'])
+    education = np.random.choice(['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'])
+    educationno = np.random.randint(1, 16)
+    maritalstatus = np.random.choice(['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'])
+    occupation = np.random.choice(['Tech-support', 'Craft-repair', 'Other-service', 'Sales', 'Exec-managerial', 'Prof-specialty', 'Handlers-cleaners', 'Machine-op-inspct', 'Adm-clerical', 'Farming-fishing', 'Transport-moving', 'Priv-house-serv', 'Protective-serv', 'Armed-Forces'])
+    relationship = np.random.choice(['Wife', 'Own-child', 'Husband', 'Not-in-family', 'Other-relative', 'Unmarried'])
+    race = np.random.choice(['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'])
+    sex = np.random.choice(['Female', 'Male'])
+    capitalgain = np.random.randint(0, 10000)
+    capitalloss = np.random.randint(0, 5000)
+    hoursperweek = np.random.randint(1, 100)
+    native = np.random.choice(['United-States', 'Cambodia', 'England', 'Puerto-Rico', 'Canada', 'Germany', 'Outlying-US(Guam-USVI-etc)', 'India', 'Japan', 'Greece', 'South', 'China', 'Cuba', 'Iran', 'Honduras', 'Philippines', 'Italy', 'Poland', 'Jamaica', 'Vietnam', 'Mexico', 'Portugal', 'Ireland', 'France', 'Dominican-Republic', 'Laos', 'Ecuador', 'Taiwan', 'Haiti', 'Columbia', 'Hungary', 'Guatemala', 'Nicaragua', 'Scotland', 'Thailand', 'Yugoslavia', 'El-Salvador', 'Trinadad&Tobago', 'Peru', 'Hong', 'Holand-Netherlands'])
+    salary = np.random.choice(['<=50K', '>50K'])
+    data.append([age, workclass, education, educationno, maritalstatus, occupation, relationship, race, sex, capitalgain, capitalloss, hoursperweek, native, salary])
+
+df = pd.DataFrame(data, columns=["age", "workclass", "education", "educationno", "maritalstatus", "occupation", "relationship", "race", "sex", "capitalgain", "capitalloss", "hoursperweek", "native", "salary"])
+df.to_csv("SalaryData.csv", index=False)
+
+# To remove duplicate elements in a dataset
+import pandas as pd
+
+df = pd.read_csv('your_file.csv')
+df.drop_duplicates(inplace=True)
+df.to_csv('new_file.csv', index=False)
